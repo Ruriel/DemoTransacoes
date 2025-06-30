d48cdd8d55f8673419aef5079ab04d1c7d63a2cc
Ruriel <ruriel.santos@gmail.com>
2025-06-30 09:35:41 -0300

    Commit inicial

---
+++ src/main/java/br/itau/unibanco/example/transacoes/DemoTransacoes/DemoTransacoesApplication.java
@@ -0,0 +1,13 @@
+package br.itau.unibanco.example.transacoes.DemoTransacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTransacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTransacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/itau/unibanco/example/transacoes/DemoTransacoes/DemoTransacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.itau.unibanco.example.transacoes.DemoTransacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTransacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
